2690bb9b2540dfaa948868359dfc23fa2afd567d
Lee <503275769@qq.com>
2023-10-23 17:34:46 +0800

    Add files via upload
    init park here

---
+++ content/park here.md
@@ -0,0 +1,5 @@
+Title: My First Review
+Date: 2023-10-23 17:06
+Category: Review
+
+Follow me to enjoy all kinds of parks.
---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+AUTHOR = 'lele'
+SITENAME = 'park here'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Africa/Abidjan'
+
+DEFAULT_LANG = 'Chinese (Simplified)'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         )
+
+# Social widget
+SOCIAL = (('github', 'https://github.com/foxlele2014'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
